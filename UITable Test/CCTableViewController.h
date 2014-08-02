9afcc5c6cef92621c605b00cdc0711deb11c6f22
Eric Croom <ericcroom@Eric-Crooms-MacBook-Pro.local>
2014-08-02 15:38:03 -0700

    first commit

--- UITable Test/CCTableViewController.h
+++ UITable Test/CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  CCViewController.h
+//  CCTableViewController.h
 //  UITable Test
 //
 //  Created by Eric Croom on 8/2/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface CCTableViewController : UITableViewController
 
 @end
